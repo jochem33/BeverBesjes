981fc7c412390d67f5fcc5419c114199211236a4
Midas Scheffers <midassceffers@gmail.com>
2019-05-29 12:54:42 +0200

    [Add] scripts to player and cam to invoke base movement

--- Beverbesjes/Assets/MoveScript.cs
+++ Beverbesjes/Assets/MoveScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
+    public Camera cam;
+    public Rigidbody rb;
+    public int speed;
+    public int maxSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class MoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * speed);
+        }
         
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * -speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * -speed);
+        }
+
+        if (!Input.anyKeyDown)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector3(maxSpeed, 0 ,0);
+        }
+        if (rb.velocity.z > maxSpeed)
+        {
+            rb.velocity = new Vector3(0, 0, maxSpeed);
+        }
+
     }
 }
---
+++ Beverbesjes/Assets/camMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMove : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        float clampedMove = Mathf.Clamp(pitch, -70.0f, 70.0f);
+
+        transform.eulerAngles = new Vector3(clampedMove, yaw, 0.0f);
+    }
+}
