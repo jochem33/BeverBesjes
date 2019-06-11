e014814f916c8dba63728824e3eaa4126ea98ee6
Midas Scheffers <midassceffers@gmail.com>
2019-06-11 15:12:08 +0200

    [Add] make pickup script

--- Beverbesjes/Assets/scripts/MoveScript.cs
+++ Beverbesjes/Assets/scripts/MoveScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
@@ -19,36 +17,47 @@ public class MoveScript : MonoBehaviour
     void Update()
     {
         
-
+        //get inputs en convert to movement
         if (Input.GetKey("w"))
         {
-            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * speed);
+            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z).normalized * speed * Time.deltaTime);
         }
         
         if (Input.GetKey("d"))
         {
-            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * speed);
+            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z).normalized * speed * Time.deltaTime);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * -speed);
+            rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z).normalized * -speed * Time.deltaTime);
         }
         if (Input.GetKey("s"))
         {
-            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * -speed);
+            rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z).normalized * -speed * Time.deltaTime);
         }
 
+        //if no key pressed stop moving
         if (!Input.anyKeyDown)
         {
             rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
+
+        //enforce max speed
         if (rb.velocity.x > maxSpeed)
         {
             rb.velocity = new Vector3(maxSpeed, 0 ,0);
         }
+        if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector3(-maxSpeed, 0, 0);
+        }
         if (rb.velocity.z > maxSpeed)
         {
             rb.velocity = new Vector3(0, 0, maxSpeed);
+        }
+        if (rb.velocity.z < -maxSpeed)
+        {
+            rb.velocity = new Vector3(0, 0, -maxSpeed);
         }
 
     }
---
+++ Beverbesjes/Assets/scripts/PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Camera cam;
+    
+
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            if (hit.transform.tag == "pickable")
+            {
+                print("dit kan ik oppakken");
+            }
+        else
+            print("I'm looking at nothing!");
+
+        
+    }
+}
