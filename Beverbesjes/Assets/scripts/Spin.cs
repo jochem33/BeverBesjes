c3234d6a7b1007d224464a37ed8677577ddacf42
Berkoder <37175347+Berkoder@users.noreply.github.com>
2019-06-18 15:30:17 +0200

    Menu en sounds added

---
+++ Beverbesjes/Assets/scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+  
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime, 0));
+    }
+}
