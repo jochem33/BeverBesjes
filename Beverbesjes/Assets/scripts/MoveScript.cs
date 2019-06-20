03be88f5471918bde12ee4d1b8700b5545362596
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-06-20 14:33:48 +0200

    [merge] foodmeter into development branch

--- Beverbesjes/Assets/scripts/MoveScript.cs
+++ Beverbesjes/Assets/scripts/MoveScript.cs
@@ -1,41 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveScript : MonoBehaviour
 {
+
     public Camera cam;
     public Rigidbody rb;
+    public RectTransform rt;
     public int speed;
     public int maxSpeed;
+    public RectTransform deathScreen;
+    public float deathSpeedForwards = -0.00125f;
+    public float deathSpeedLeftRight = -0.000625f;
+    public float deathSpeedBackwards = -0.0006f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
 
         if (Input.GetKey("w"))
         {
             rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(deathSpeedForwards, 0, 0);
+            }
+            else
+            {
+                Destroy(rb);
+                if (deathScreen.transform.localPosition.x > 0)
+                {
+                    deathScreen.localPosition = new Vector3(0, 0, 0);
+                }
+            }
+
         }
-        
+
         if (Input.GetKey("d"))
         {
             rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(deathSpeedLeftRight, 0, 0);
+            }
         }
         if (Input.GetKey("a"))
         {
             rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * -speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(deathSpeedLeftRight, 0, 0);
+            }
         }
         if (Input.GetKey("s"))
         {
             rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * -speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(deathSpeedBackwards, 0, 0);
+            }
         }
 
         if (!Input.anyKeyDown)
@@ -44,7 +75,7 @@ public class MoveScript : MonoBehaviour
         }
         if (rb.velocity.x > maxSpeed)
         {
-            rb.velocity = new Vector3(maxSpeed, 0 ,0);
+            rb.velocity = new Vector3(maxSpeed, 0, 0);
         }
         if (rb.velocity.z > maxSpeed)
         {
