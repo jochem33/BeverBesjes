6c6b499b5645dedc7ecfa06e1ecaf7e7047c8cc7
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-06-20 09:11:35 +0200

    [change] code cleanup and placeholder apple removed

--- Beverbesjes/Assets/scripts/ChangeCrosshair2.cs
+++ Beverbesjes/Assets/scripts/ChangeCrosshair2.cs
@@ -16,49 +16,28 @@ public class ChangeCrosshair2 : MonoBehaviour
 
     void Start()
     {
-        //canvasImage = GetComponent<Image>();
-        print("AB");
+        //code
     }
 
     void Update()
     {
-        print("dit kan ik niet oppakken");
-
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 7))
         {
             if (hit.transform.tag == "pickable")
             {
-                print("dit kan ik oppakken");
-                //SetImage2();
-                //Debug.Log(hit.collider.gameObject.name);
                 canvasImage.sprite = crosshairActive;
             }
             else
             {
-                //SetImage1();
-                print("dit kan ik niet oppakken");
                 canvasImage.sprite = crosshairInactive;
             }
         }
         else
         {
-            //SetImage1();
-            print("dit kan ik niet oppakken");
             canvasImage.sprite = crosshairInactive;
         }
 
-    }
-
-
-    public void SetImage1() //method to set our first image
-    {
-        canvasImage.sprite = crosshairInactive;
-    }
-
-    public void SetImage2()
-    {
-        canvasImage.sprite = crosshairActive;
     }
 }
