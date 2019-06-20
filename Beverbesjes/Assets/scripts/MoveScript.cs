4c637f2dfb3a4a858b16be3a25e3f6e25395bda8
Luuk <luuknoverraz@gmail.com>
2019-06-20 08:52:07 +0200

    [ADD] You can now die

--- Beverbesjes/Assets/scripts/MoveScript.cs
+++ Beverbesjes/Assets/scripts/MoveScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveScript : MonoBehaviour {
     
@@ -9,11 +10,11 @@ public class MoveScript : MonoBehaviour {
     public RectTransform rt;
     public int speed;
     public int maxSpeed;
+    public RectTransform deathScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-   
     }
 
     // Update is called once per frame
@@ -26,11 +27,15 @@ public class MoveScript : MonoBehaviour {
             rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * speed);
             if (rt.transform.localScale.x >= 0)
             {
-                rt.localScale += new Vector3(-0.1f, 0, 0);
+                rt.localScale += new Vector3(-0.00125f, 0, 0);
             }
             else
             {
                 Destroy(rb);
+                if (deathScreen.transform.localPosition.x > 0)
+                {
+                    deathScreen.localPosition = new Vector3(0, 0, 0);
+                }
             }
             
         }
@@ -38,14 +43,26 @@ public class MoveScript : MonoBehaviour {
         if (Input.GetKey("d"))
         {
             rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(-0.000625f, 0, 0);
+            }
         }
         if (Input.GetKey("a"))
         {
             rb.AddForce(new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * -speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(-0.000625f, 0, 0);
+            }
         }
         if (Input.GetKey("s"))
         {
             rb.AddForce(new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * -speed);
+            if (rt.transform.localScale.x >= 0)
+            {
+                rt.localScale += new Vector3(-0.0006f, 0, 0);
+            }
         }
 
         if (!Input.anyKeyDown)
