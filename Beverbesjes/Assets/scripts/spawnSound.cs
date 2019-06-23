6af5667053ebe9f1732c683900643a55e54128a4
CoderClassLuuk <luuk.noverraz@coderclass.nl>
2019-06-23 21:52:13 +0200

    [ADD] Pop sound effect when spawning

---
+++ Beverbesjes/Assets/scripts/spawnSound.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnSound : MonoBehaviour
+{
+     AudioSource m_MyAudioSource;
+    //Play the music
+    bool m_Play;
+    //Detect when you use the toggle, ensures music isn’t played multiple times
+    bool m_ToggleChange;
+    void Start()
+    {
+        //Fetch the AudioSource from the GameObject
+        m_MyAudioSource = GetComponent<AudioSource>();
+        //Ensure the toggle is set to true for the music to play at start-up
+        m_Play = true;
+    }
+
+    void Update()
+    {
+        //Check to see if you just set the toggle to positive
+        if (m_Play == true && m_ToggleChange == true)
+        {
+            //Play the audio you attach to the AudioSource component
+            m_MyAudioSource.Play();
+            //Ensure audio doesn’t play more than once
+            m_ToggleChange = false;
+        }
+        //Check if you just set the toggle to false
+        if (m_Play == false && m_ToggleChange == true)
+        {
+            //Stop the audio
+            m_MyAudioSource.Stop();
+            //Ensure audio doesn’t play more than once
+            m_ToggleChange = false;
+        }
+    }
+
+}
