f1aa6e75a856bc98b362d0b75e8336602f74a401
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-06-25 14:39:15 +0200

    [merge] menu branche into development branche

---
+++ Beverbesjes/Assets/scripts/Switch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Switch : MonoBehaviour
+{
+
+    public void ChangeScene(string SceneName) {
+        SceneManager.LoadScene(SceneName);
+    }
+
+
+}
